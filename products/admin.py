b822426944a72e1ab3315456312c1aafc89b77af
Nidhi Sharma <nidhinavhal@gmail.com>
2024-05-31 08:37:14 +0530

    Initial commit

---
+++ products/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Category)
+
+admin.site.register(Coupon)
+
+class ProductImageAdmin(admin.StackedInline):
+    model = ProductImage
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['product_name', 'price']
+    inlines = [ProductImageAdmin]
+
+@admin.register(ColorVariant)
+class ColorVariantAdmin(admin.ModelAdmin):
+    list_display = ['color_name', 'price']
+    model = ColorVariant
+
+@admin.register(SizeVariant)
+class SizevariantAdmin(admin.ModelAdmin):
+    list_display = ['size_name', 'price']
+    model = SizeVariant
+
+admin.site.register(Product, ProductAdmin)
+
+admin.site.register(ProductImage)
